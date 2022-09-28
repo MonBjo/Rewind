4a44b1badf5c252868de90f939d59a0f45545e31
Monica Björk <m.bjork95@gmail.com>
2022-09-28 09:13:06 +0200

    Sort games by date

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -3,12 +3,22 @@ import GameCard from '../componenets/GameCard';
 import data from '../assets/games.json';
 
 function LandingPage() {
+  let gamesSortedByDate: any = data.games.sort((a, b) => {
+    if (a.date > b.date) {
+      return -1;
+    }
+    else if (a.date < b.date) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
   
   return (
     <section className="LandingPage page">
       <Header />
       <p>Landingpage</p>
-      {data.games.map(game => {
+      {gamesSortedByDate.map((game: { type: string; date: string; players: any; }) => {
         return (
           <GameCard type={game.type} date={game.date} players={game.players} />
         )

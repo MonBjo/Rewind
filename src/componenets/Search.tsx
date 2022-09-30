ba5a2946e7e1e7e51dd2f5c45adfda2c178fc0ed
Monica Björk <m.bjork95@gmail.com>
2022-09-30 20:08:39 +0200

    search almost finished

--- src/componenets/Search.tsx
+++ src/componenets/Search.tsx
@@ -1,17 +1,25 @@
 import { useState } from 'react';
 import './search.scss';
 
-function Search(props: { search: any; setSearch: any; }) {
-  const { search, setSearch } = props;
+function Search(props: { setSearch: any; setOnlyWins: any; }) {
+  const { setSearch,  setOnlyWins } = props;
 
   function doSearch(event: any) {
     console.log(event.target.value.toLowerCase());
     setSearch(event.target.value.toLowerCase());
   }
 
+  function getOnlyWins(event: { target: { checked: boolean | ((prevState: boolean) => boolean); }; }) {
+    setOnlyWins(event.target.checked);
+  }
+
   return (
     <section className="search">
       <input type="text" placeholder='Sök på namn, datum eller match-typ' onChange={doSearch} />
+      <section>
+          <label htmlFor="winsOnly" >Visa endast vinster: </label>
+          <input type="checkbox" id="winsOnly" onChange={getOnlyWins} />
+        </section>
     </section>
   );
 }
--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -6,19 +6,20 @@ import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
   const [search, setSearch] = useState('');
+  const [onlyWins, setOnlyWins] = useState(false);
   const navigate = useNavigate();
 
   let games = JSON.parse(localStorage.games);
   let wins: number = 0;
   let playerWin: string = "";
   let winsOf: number = 0;
-  let winsOfMax: number = 10;
-  
+  let maxWins: number = 10;
+
   function navAddGame() {
     navigate('/AddGame');
   } 
   
-  let gamesSortedByDate: any = games.games.sort((a: { date: number; }, b: { date: number; }) => {
+  let gamesToDisplay: any = games.games.sort((a: { date: number; }, b: { date: number; }) => {
     if (a.date > b.date) {
       return -1;
     } else if (a.date < b.date) {
@@ -28,42 +29,36 @@ function LandingPage() {
     }
   });
 
-  gamesSortedByDate = gamesSortedByDate.filter((game: { players: any; type: string; date: string; }) => {
+  gamesToDisplay = gamesToDisplay.filter((game: { players: any; type: string; date: string; }) => {
     if( game.type.toLowerCase().includes(search.toLowerCase()) || game.date.toLowerCase().includes(search.toLowerCase()) ) {
       return game;
     } 
 
     for(let player of game.players) {
       if (player.name.toLowerCase().includes(search.toLowerCase())) {
-        
-        // if(player.won && player.name.toLowerCase() == search.toLowerCase()) {
         if(player.won) {
           wins++;
         }
-        // if(winsOf < winsOfMax && player.name.toLowerCase() == search.toLowerCase()) {
-        if(winsOf < winsOfMax) {
+        if(winsOf < maxWins) {
           winsOf++;
         }
-
-        // Debug data
-        // console.log("wins", wins);
-        // console.log("winsOf", winsOf);
-        // console.log("player.name",player.name);
         
         playerWin = player.name;
-        return game;
+        console.log("onlyWins", onlyWins);
+        if(onlyWins && player.won) {
+          return game;
+        } else if(!onlyWins) {
+          return game;
+        }
       }
     }
   });
+  
 
   function displayWins() {
-    if(playerWin.toLowerCase() == search.toLowerCase() || playerWin.length > 3) {
+    if(playerWin.length != 0 && playerWin.toLowerCase() == search.toLowerCase() || playerWin.length > 3) {
       return (
-        <section>
-          <p>{playerWin} har vunnit {wins} av dom {winsOf} senaste matcherna.</p> 
-          <label htmlFor="winsOnly" >Visa endast vinster: </label>
-          <input type="checkbox" id="winsOnly" />
-        </section>
+        <p>{playerWin} har vunnit {wins} av dom {winsOf} senaste matcherna.</p> 
       );
     }
   }
@@ -71,10 +66,10 @@ function LandingPage() {
   return (
     <section className="LandingPage page">
       <Header />
-      <Search search={search} setSearch={setSearch} />
+      <Search setSearch={setSearch} setOnlyWins={setOnlyWins} />
       <button onClick={ navAddGame }>Lägg till ny match</button>
       {displayWins()}
-      {gamesSortedByDate.map((game: { type: string; date: string; players: any; }, index: number) => {
+      {gamesToDisplay.map((game: { type: string; date: string; players: any; }, index: number) => {
         return (
           <GameCard type={game.type} date={game.date} players={game.players} key={index} />
         )
